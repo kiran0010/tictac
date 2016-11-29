f39dcd9a340311744bfdc33bda919a2a634377ed
kiran0010 <totan0010@gmail.com>
2016-11-30 03:12:04 +0530

    my code

---
+++ tictac.js
@@ -0,0 +1,116 @@
+var count=0;
+function myFunction(){if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage').src='y.jpg';
+    }
+    count=count+1;
+    result();
+       }
+function myFunction1(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage1').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage1').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+function myFunction2(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage2').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage2').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+function myFunction3(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage3').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage3').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+function myFunction4(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage4').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage4').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+function myFunction5(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage5').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage5').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+function myFunction6(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage6').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage6').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+function myFunction7(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage7').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage7').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+function myFunction8(){
+
+    
+        if(count===0 || count===2 || count===4 || count===6 || count===8){
+    document.getElementById('myImage8').src='x.jpg';
+    }
+    else{
+    document.getElementById('myImage8').src='y.jpg';
+    }
+    count=count+1;
+    result();
+}
+
+ function result(){
+    if(count<7)
+    {
+document.getElementById("demo").innerHTML = "You Win";
+    }
+    else{
+        document.getElementById("demo").innerHTML = "Tie";
+    }
+ }
